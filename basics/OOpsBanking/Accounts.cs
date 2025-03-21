c7a839db22ad8732538143b092c9e24b85215231
Sreehari Aranghat <sreehariaranghat@Sreeharis-MacBook-Pro.local>
2025-03-21 12:13:13 +0530

    Added Virtual MEthods

--- basics/OOpsBanking/Accounts.cs
+++ basics/OOpsBanking/Accounts.cs
@@ -1,7 +1,26 @@
 public abstract class Account {
     public int AccountNumber { get; set; }
     public Customer AccountHolder { get; set; }
+
+    public double InterestRate { get; protected set; }
     public double Balance { get; set; }
+
+    //1. Scenario1 : The function is COMMON for all child classes
+    public double CalculateInterest() {
+        return Balance * InterestRate / 100;
+    }
+
+    //2. Scenario2 : The function is DIFFERENT for all child classes
+    //1. Abstract methoed will not have a body
+    //2. Child classes must implement the method
+    public abstract void Deposit(double amount);
+
+    //3. Scenario3 : The function is COMMON for some child classes and DIFFERENT for others
+    public virtual void DisplayAccountDetails() {
+        Console.WriteLine($"Account Number: {AccountNumber}");
+        Console.WriteLine($"Balance: {Balance}");
+        Console.WriteLine($"Interest Rate: {InterestRate}");
+    }
 }
 
 //Savings account inherits from Account
@@ -11,12 +30,23 @@ public abstract class SavingsAccount : Account {
     //2. Outside world must not be able to set the minimum balance
     //3. Outside world must be able to read the minimum balance
     public double MonthlyMinimumBalance { get; protected set; }
+
+    public override void Deposit(double amount)
+    {
+        Balance += amount;
+    }
 }
 
 public class RegularSavingsAccount : SavingsAccount {
 
     public RegularSavingsAccount() {
         MonthlyMinimumBalance = 10000;
+        InterestRate = 5.5f;
+    }
+
+    override public void DisplayAccountDetails() {
+        base.DisplayAccountDetails();
+        Console.WriteLine($"Monthly Minimum Balance: {MonthlyMinimumBalance}");
     }
 }
 
@@ -24,11 +54,18 @@ sealed public class SalarySavingsAccount : SavingsAccount {
 
     public SalarySavingsAccount() {
         MonthlyMinimumBalance = 0;
+        InterestRate = 4.5f;
     }
 }
 
 
 sealed public class LoanAccount : Account {
   
-    
+    public LoanAccount() {
+        InterestRate = 12.5f;
+    }
+
+    override public void Deposit(double amount) {
+        Balance -= amount;
+    }
 }
--- basics/OOpsBanking/Program.cs
+++ basics/OOpsBanking/Program.cs
@@ -2,13 +2,24 @@
     public static void Main(string[] args) {
 
         var savingsAccount = new RegularSavingsAccount();
+        savingsAccount.Balance = 10000;
         var salarySavingsAccount = new SalarySavingsAccount();
 
-        Console.WriteLine(savingsAccount.MonthlyMinimumBalance);
-        Console.WriteLine(salarySavingsAccount.MonthlyMinimumBalance);
-       
+        //savingsAccount.Deposit(1000);
 
+        savingsAccount.DisplayAccountDetails();
+        salarySavingsAccount.DisplayAccountDetails();
+
+        //savingsAccount.CalculateInterest(); //5.5%
+        //salarySavingsAccount.CalculateInterest(); //4.5%
         var loanAccount = new LoanAccount();
-  
+        loanAccount.Balance = 10000;
+        loanAccount.Deposit(1000);
+        loanAccount.DisplayAccountDetails();
+       // loanAccount.CalculateInterest(); //12.5%
+    
+       // Console.WriteLine(savingsAccount.Balance);
+       // Console.WriteLine(loanAccount.Balance);
+
     }
 }
