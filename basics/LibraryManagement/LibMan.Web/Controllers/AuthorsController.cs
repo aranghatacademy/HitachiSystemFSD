3a6fc8eaca1dcd99f6323f62b5aedafdcf9df5da
Sreehari Aranghat <sreehariaranghat@Sreeharis-MacBook-Pro.local>
2025-04-03 10:33:29 +0530

    Added Author nav with route

--- basics/LibraryManagement/LibMan.Web/Controllers/AuthorsController.cs
+++ basics/LibraryManagement/LibMan.Web/Controllers/AuthorsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using LibMan.Web.Db;
 namespace LibMan.Web.Controllers
 {
+    [Route("authors")]
     public class AuthorsController : Controller
     {
         private readonly BookDbContext _context;
@@ -13,8 +14,10 @@ namespace LibMan.Web.Controllers
         }
         // GET: AuthorsController
         [HttpGet("{author}")]
-        public ActionResult Index(string author)
+        public ActionResult Index([FromRoute] string author)
         {
+            _logger.LogInformation("Author: {author}", author);
+            ViewBag.Author = author;
             var books = _context.Books.Where(b => b.Author == author).ToList();
             return View(books);
         }
