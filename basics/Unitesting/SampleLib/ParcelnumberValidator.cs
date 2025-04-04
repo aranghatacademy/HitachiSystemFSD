7efa413425507d8ee66e490c28b7609c87d22fc6
Sreehari Aranghat <sreehariaranghat@Sreeharis-MacBook-Pro.local>
2025-04-04 10:40:25 +0530

    Added Data

---
+++ basics/Unitesting/SampleLib/ParcelnumberValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace SampleLib;
+
+public static class ParcelnumberValidator
+{
+    public static bool IsValid(string parcelnumber)
+    {
+         var isValid = parcelnumber.StartsWith("BLR",StringComparison.OrdinalIgnoreCase) && parcelnumber.Length == 5;
+         return isValid;
+    }
+}
---
+++ basics/Unitesting/SampleLibTest/ParcelValidatorTest.cs
@@ -0,0 +1,55 @@
+﻿using SampleLib;
+
+namespace SampleLibTest;
+
+public class ParcelnumberValidatorTest
+{
+    [SetUp]
+    //Any dependencies that need to be created will be created here
+    public void Setup()
+    {
+    }
+
+    [Test]
+    //MethodToTest_Scenario_ExpectedResult
+    public void IsValid_WhenGivenValidParcelnumber_ReturnsTrue()
+    {
+        //Arrange
+        var parcelnumber = "BLR12";
+
+        //Act
+        var result = ParcelnumberValidator.IsValid(parcelnumber);
+
+        //Assert
+        //Check if the return value of ISValid is true if you pass BLR12
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void IsValid_WhenGivenInvalidParcelnumber_ReturnsFalse()
+    {
+        //Arrange
+        var parcelnumber = "BLR123"; //Invalid because its length is not 5
+
+        //Act
+        var result = ParcelnumberValidator.IsValid(parcelnumber);
+
+        //Assert
+        Assert.That(result, Is.False);
+    }
+
+
+    [Test]
+    public void IsValid_WhenGivenLowerCaseParcelnumber_ReturnsFalse()
+    {
+        //Arrange
+        var parcelnumber = "blr12"; //Invalid because its length is not 5
+
+        //Act
+        var result = ParcelnumberValidator.IsValid(parcelnumber);
+
+        //Assert
+        Assert.That(result, Is.True);
+    }
+
+}
